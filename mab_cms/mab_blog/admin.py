0bb709712ec3f47825dbd3c2b1b5753e217e5e79
Arline Centeno <arline.centeno@gmail.com>
2017-07-18 20:08:51 -0400

    renames model Blog->Post)

--- mab_cms/mab_blog/admin.py
+++ mab_cms/mab_blog/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from mab_blog.models import Blog, Category
+from mab_blog.models import Post, Category
 
-class BlogAdmin(admin.ModelAdmin):
+class PostAdmin(admin.ModelAdmin):
     exclude = ['posted']
     prepopulated_fields = {'slug': ('title',)}
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
 
-admin.site.register(Blog, BlogAdmin)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category, CategoryAdmin)
--- mab_cms/mab_blog/models.py
+++ mab_cms/mab_blog/models.py
@@ -3,13 +3,13 @@ from django.db.models import permalink
 from filer.fields.image import FilerImageField
 from djangocms_text_ckeditor.fields import HTMLField
 
-class Blog(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
     body = HTMLField()
     posted = models.DateField(db_index=True, auto_now_add=True)
     category = models.ForeignKey('mab_blog.Category')
-    background_image = FilerImageField(null=True, blank=True)
+    cover_image = FilerImageField(null=True, blank=True)
 
     def __str__(self):
         return '%s' % self.title
